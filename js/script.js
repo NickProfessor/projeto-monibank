8457aec7db782358265f8a53d55fd2d001d544fe
Nickollas <nickollassilva90@gmail.com>
2024-04-17 01:17:56 -0300

    Validando a idade do usuario com javaScript

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 import ehUmCpf from "./valida-cpf.js";
+import ehMaiorDeIdade from "./valida-idade.js";
 const camposDoFormulario = document.querySelectorAll('[required]');
 
 camposDoFormulario.forEach((campo) => {
@@ -9,4 +10,8 @@ function verificaCampo(campo) {
     if (campo.name == "cpf" && campo.value.length >= 11){
         ehUmCpf(campo)
     }
+    if(campo.name == "aniversario" && campo.value != ""){
+        ehMaiorDeIdade(campo);
+    }
+    
 }
--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,7 +1,12 @@
 export default function ehUmCpf(campo){
     const cpf = campo.value.replace(/\.|-/g, '');
-    validaNumerosRepetidos(cpf) 
-
+    
+    
+    if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        console.log("Esse cpf nao existe")
+    }else{
+        console.log("Esse cpf existe")
+    }
     console.log(validaNumerosRepetidos(cpf))
 }
 
@@ -20,4 +25,40 @@ function validaNumerosRepetidos(cpf){
     ]
 
     return numeroRepetidos.includes(cpf);
+}
+
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for (let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[9];
+}
+
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[10];
 }
